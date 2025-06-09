25ba8e82fb61ce14dfc8fee23660209bc8003edc
Martin <martin@cosmoswide.com.br>
2025-06-09 18:16:33 -0300

    Corrige função de printar tabuleiro; Adiciona animação para ataques;

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
 #Projeto batalha naval - Raciocínio Algorítmico
 
-import time, random
+import time, random, os, platform
 
 COORDS_V = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
+def limpar_terminal():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
 
 def gerar_tabuleiro(altura, largura):
     matriz = ['🟦'] * altura
@@ -12,13 +17,16 @@ def gerar_tabuleiro(altura, largura):
     return matriz
 
 
-def printar_tabuleiro(matriz_tabuleiro, largura, altura, coords_h, COORDS_V):
+def printar_tabuleiro(matriz_tabuleiro, largura, altura, coords_h, coords_v):
+    linha_matriz = ''
+
     for linha in range(altura):
-        linha_matriz += COORDS_V[linha] + '  '
+        linha_matriz += coords_v[linha] + '  '
         for coluna in range(largura):
             linha_matriz += matriz_tabuleiro[linha][coluna] + '  '
         linha_matriz += '  '
         print(linha_matriz)
+        linha_matriz = ''
     print(coords_h)
 
 
@@ -36,22 +44,76 @@ def posicionar_unidades_tabuleiro(jogador, tabuleiro, largura, altura, coords_h,
     if (jogador == 'jogador'):
         for i in range(5) : 
             print('Escolha a posição da unidade: ')
-            pos_h = input('Digite uma letra entre A - J: ').upper()
-            pos_h_convertido = converter_posicao(pos_h, largura, coords_v)
-            pos_v = input(f'Digite um número entre 1 - {largura}: ')
+            pos_v = input('Digite uma letra entre A - J: ').upper()
+            pos_v_convertido = converter_posicao(pos_v, largura, coords_v)
+            pos_h = input(f'Digite um número entre 1 - {largura}: ')
             
-            print(pos_v, pos_h_convertido)
+            print(pos_h, pos_v_convertido)
             
-            tabuleiro[int(pos_v)-1][int(pos_h_convertido)-1] = '🛶'
+            tabuleiro[int(pos_v_convertido)-1][int(pos_h)-1] = '🛶'
             
         printar_tabuleiro(tabuleiro, largura, altura, coords_h, coords_v)
     elif (jogador == 'computador') :
         for i in range(5):
-            pos_h = random.randint(1, largura)
-            pos_v = random.randint(1, altura)
-            tabuleiro[int(pos_v)-1][int(pos_h)-1] = '🛶'
-        printar_tabuleiro(tabuleiro, largura, altura, coords_h, coords_v)
+            pos_v = random.randint(1, largura)
+            pos_h = random.randint(1, altura)
+            tabuleiro[int(pos_h)-1][int(pos_v)-1] = '🛶'
+        # printar_tabuleiro(tabuleiro, largura, altura, coords_h, coords_v)
 
+
+def contar_barcos(tabuleiro):
+    """Conta quantos barcos restam no tabuleiro."""
+    return sum(linha.count('🛶') for linha in tabuleiro)
+
+
+def atacar(tabuleiro_alvo, largura, altura, coords_v, jogador):
+    while True:
+        if jogador == "Jogador":
+            pos_v = input('Escolha a linha (letra A-J): ').upper()
+            pos_v_convertido = converter_posicao(pos_v, altura, coords_v)
+            pos_h = input(f'Escolha a coluna (1-{largura}): ')
+            try:
+                linha = int(pos_v_convertido) - 1
+                coluna = int(pos_h) - 1
+                if tabuleiro_alvo[linha][coluna] in ['💥', '❌']:
+                    print("Você já atacou essa posição. Escolha outra.")
+                    continue
+                break
+            except (ValueError, IndexError):
+                print("Coordenada inválida. Tente novamente.")
+        else:
+            linha = random.randint(0, altura - 1)
+            coluna = random.randint(0, largura - 1)
+            if tabuleiro_alvo[linha][coluna] in ['💥', '❌']:
+                continue
+            break
+
+    if tabuleiro_alvo[linha][coluna] == '🛶':
+        for frame in ["💣", "💥", "🔥"]:
+            tabuleiro_alvo[linha][coluna] = frame
+            limpar_terminal()
+            print(f"Ataque do {jogador}.")
+            printar_tabuleiro(tabuleiro_alvo, largura, altura, '', coords_v)
+            time.sleep(0.7)
+        tabuleiro_alvo[linha][coluna] = '💥'
+        limpar_terminal()
+        printar_tabuleiro(tabuleiro_alvo, largura, altura, '', coords_v)
+        print(f"{jogador} acertou um barco!")
+        return True
+    else:
+        for frame in ["💣", "🌊", "❌"]:
+            tabuleiro_alvo[linha][coluna] = frame
+            limpar_terminal()
+            print(f"Ataque do {jogador}.")
+            printar_tabuleiro(tabuleiro_alvo, largura, altura, '', coords_v)
+            time.sleep(0.7)
+        tabuleiro_alvo[linha][coluna] = '❌'
+        limpar_terminal()
+        printar_tabuleiro(tabuleiro_alvo, largura, altura, '', coords_v)
+        print(f"{jogador} errou o ataque.")
+        return False
+    
+    
 #START
 coords_h = ''
 # COORDS_V = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
@@ -76,8 +138,27 @@ tabuleiro_jogador = gerar_tabuleiro(altura, largura)
 printar_tabuleiro(tabuleiro_jogador, largura, altura, coords_h, COORDS_V)
 tabuleiro_computador = gerar_tabuleiro(altura, largura)
 posicionar_unidades_tabuleiro('jogador', tabuleiro_jogador, largura, altura, coords_h, COORDS_V)
+print('')
 posicionar_unidades_tabuleiro('computador', tabuleiro_computador, largura, altura, coords_h, COORDS_V)
 
+while True:
+    print("\nSeu tabuleiro:")
+    printar_tabuleiro(tabuleiro_jogador, largura, altura, coords_h, COORDS_V)
+    print("\nTabuleiro do computador:")
+    printar_tabuleiro(tabuleiro_computador, largura, altura, coords_h, COORDS_V)
+
+    print("\nSua vez de atacar!")
+    atacar(tabuleiro_computador, largura, altura, COORDS_V, "Jogador")
+    if contar_barcos(tabuleiro_computador) == 0:
+        print("Parabéns! Você venceu!")
+        break
+
+    print("\nVez do computador atacar!")
+    atacar(tabuleiro_jogador, largura, altura, COORDS_V, "Computador")
+    if contar_barcos(tabuleiro_jogador) == 0:
+        print("O computador venceu!")
+        break
+        
 #END
 print("Jogo desenvolvido por: Elgson Nascimento, Maria Pietra, Martin Romão, Hikari Hayashida")
 print("Obrigado por jogar!!")
